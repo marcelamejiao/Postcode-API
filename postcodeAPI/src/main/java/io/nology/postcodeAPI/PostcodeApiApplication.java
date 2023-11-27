93a852fcaf56c8e82977039e18dfd4087d16beb5
marcelamejiao <mejiao24@hotmail.com>
2023-11-27 15:56:55 +1100

    initial spring set up project

---
+++ postcodeAPI/src/main/java/io/nology/postcodeAPI/PostcodeApiApplication.java
@@ -0,0 +1,13 @@
+package io.nology.postcodeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostcodeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostcodeApiApplication.class, args);
+	}
+
+}
---
+++ postcodeAPI/src/test/java/io/nology/postcodeAPI/PostcodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nology.postcodeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostcodeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
